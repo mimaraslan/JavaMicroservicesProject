694b1c9d6cf20aef17b78773ea057ea78586c0e7
Mimar Aslan <mimaraslan@gmail.com>
2026-04-24 00:57:55 +0300

    DevOps
    DevOps

--- _01_terraform/_02_terraform_main_ec2/terraform.tf
+++ _01_terraform/_02_terraform_main_ec2/terraform.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "my-devops-lab-bucket-2"
+    bucket = "my-devops-lab-bucket-1"
     key    = "ec2/terraform.tfstate"
     region = "us-east-1"
   }
