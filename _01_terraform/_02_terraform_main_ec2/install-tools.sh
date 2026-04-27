93828185b68f9c452e2882f60b11d40a0b0058d5
Mimar Aslan <mimaraslan@gmail.com>
2026-04-27 18:41:27 +0300

    DevOps
    DevOps

--- _01_terraform/_02_terraform_main_ec2/install-tools.sh
+++ _01_terraform/_02_terraform_main_ec2/install-tools.sh
@@ -38,7 +38,7 @@ sudo usermod -aG docker jenkins
 sudo systemctl enable --now docker
 sudo chmod 777 /var/run/docker.sock
 
-# Docker Compose V2 (Kurulum yolu düzeltildi)
+# Docker Compose V2 Kurulum
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
