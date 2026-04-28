ae6597cb874a86b280197dfea1fccb9be3e00f8a
Mimar Aslan <mimaraslan@gmail.com>
2026-04-28 19:13:34 +0300

    EKS
    EKS

--- _01_terraform/_03_eks-terraform/variable.tf
+++ _01_terraform/_03_eks-terraform/variable.tf
@@ -3,3 +3,9 @@ variable "node_group_name" {
   type        = string
   default     = "project-node-group"
 }
+
+variable "instance_name" {
+  description = "Name of the EKS node group"
+  type        = string
+  default     = "mydemo-eks-node"
+}
