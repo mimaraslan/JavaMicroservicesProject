17330c4c777d08845a3adf495056385353f79ca2
Mimar Aslan <mimaraslan@gmail.com>
2026-04-28 20:16:31 +0300

    EKS
    EKS

--- _01_terraform/_03_eks-terraform/variable.tf
+++ _01_terraform/_03_eks-terraform/variable.tf
@@ -1,11 +1,47 @@
-variable "node_group_name" {
-  description = "Name of the EKS node group"
+# ---------------------------------------------------------
+# 1. Bölge Yapılandırması
+# ---------------------------------------------------------
+variable "aws_region" {
+  description = "Kaynakların oluşturulacağı AWS bölgesi"
   type        = string
-  default     = "project-node-group"
+  default     = "us-east-1"
+}
+
+# ---------------------------------------------------------
+# 2. EKS Cluster Değişkenleri
+# ---------------------------------------------------------
+variable "cluster_name" {
+  description = "EKS Cluster'ın adı (main.tf içindeki project-eks ile uyumlu)"
+  type        = string
+  default     = "project-eks"
+}
+
+# ---------------------------------------------------------
+# 3. Node Group ve Makine İsimlendirme
+# ---------------------------------------------------------
+variable "node_group_name" {
+  description = "EKS Node Group'un AWS panelindeki grup adı"
+  type        = string
+  default     = "project-eks-node-group"
 }
 
 variable "instance_name" {
-  description = "Name of the EKS node group"
+  description = "Worker Node makinelerinin EC2 panelinde görünecek etiketi (Tag: Name)"
   type        = string
-  default     = "mydemo-eks-node"
+  default     = "mydemo-eks-worker-node"
+}
+
+# ---------------------------------------------------------
+# 4. Node Donanım Ayarları (Opsiyonel)
+# ---------------------------------------------------------
+variable "instance_types" {
+  description = "Worker Node'lar için kullanılacak makine tipi"
+  type        = list(string)
+  default     = ["t2.large"]
+}
+
+variable "disk_size" {
+  description = "Her bir Worker Node için disk boyutu (GB)"
+  type        = number
+  default     = 20
 }
