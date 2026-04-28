cd2251ead258e4f53fcdf19db2c7f00b93de0b47
Mimar Aslan <mimaraslan@gmail.com>
2026-04-29 02:39:12 +0300

    ApiGateway
    ApiGateway

--- ApiGatewayService/src/main/java/com/mimaraslan/controller/FallbackController.java
+++ ApiGatewayService/src/main/java/com/mimaraslan/controller/FallbackController.java
@@ -9,30 +9,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/fallback")
 public class FallbackController {
 
-    // http://localhost:9091/fallback/account
+    // http://localhost:9591/fallback/account
     @GetMapping("/account")
     public  ResponseEntity<String>  fallbackAccount() {
         return ResponseEntity.ok("Account Service: Şu anda geçici olarak hizmet verememekteyiz.");
     }
 
+    // http://localhost:9592/fallback/ledger
+    @GetMapping("/ledger")
+    public  ResponseEntity<String>  fallbackLedger() {
+        return ResponseEntity.ok("Ledger Service: Şu anda geçici olarak hizmet verememekteyiz.");
+    }
 
-    // http://localhost:9092/fallback/notification
+    // http://localhost:9593/fallback/fraud
+    @GetMapping("/fraud")
+    public  ResponseEntity<String>  fallbackFraud() {
+        return ResponseEntity.ok("Fraud Service: Şu anda geçici olarak hizmet verememekteyiz.");
+    }
+
+    // http://localhost:9594/fallback/notification
     @GetMapping("/notification")
     public  ResponseEntity<String>  fallbackNotification() {
         return ResponseEntity.ok("Notification Service: Şu anda geçici olarak hizmet verememekteyiz.");
     }
 
 
-    // http://localhost:9094/fallback/fraud
-    @GetMapping("/fraud")
-    public  ResponseEntity<String>  fallbackFraud() {
-        return ResponseEntity.ok("Fraud Service: Şu anda geçici olarak hizmet verememekteyiz.");
-    }
-
-
-    // http://localhost:9096/fallback/ledger
-    @GetMapping("/ledger")
-    public  ResponseEntity<String>  fallbackLedger() {
-        return ResponseEntity.ok("Ledger Service: Şu anda geçici olarak hizmet verememekteyiz.");
-    }
 }
